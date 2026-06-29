8b06df22be7662e39b2b8dbb8a28902dfee250f1
liuliano <iulianolawrence@yahoo.com>
2026-06-29 09:39:26 -0400

    Add feature roadmap module

---
+++ features.js
@@ -0,0 +1,9 @@
+export const FEATURE_ROADMAP = [
+  'Live Pool Win Probability Dashboard',
+  'Pool Control Center',
+  'Match Simulator',
+  'What I Need to Win',
+  'Improved Mobile Bracket',
+  'ESPN/DraftKings Line Locking',
+  'Password Protected Spread Overrides'
+];
